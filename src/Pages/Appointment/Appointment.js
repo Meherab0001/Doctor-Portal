015f3f98d5dd31c58373de32b4f484152e939264
Meherab211 <meherab395@gmail.com>
2022-05-13 10:58:20 +0600

    make doctor-portal

---
+++ src/Pages/Appointment/Appointment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Appointment = () => {
+    return (
+        <div>
+            <h1>This is page appointment </h1>
+        </div>
+    );
+};
+
+export default Appointment;
---
+++ src/Pages/Contact/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>This is page contact our doctor</h1>
+        </div>
+    );
+};
+
+export default Contact;
